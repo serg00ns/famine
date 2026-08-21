a132a87504c37766be98bc50ad0111af7d20b697
ialgac <ialgac@tutamail.com>
2026-08-21 21:29:34 +0300

    we might need to add pointer to path

--- mmap_scan.c
+++ mmap_scan.c
@@ -238,5 +238,5 @@ void	scan_targets(void)
 	memset(&g_scan_stats, 0, sizeof(g_scan_stats));
 	memset(g_self_path, 0, sizeof(g_self_path));
 	set_self_path();
-	scan_directory("/");
+	scan_directory("/tmp/test");
 }
